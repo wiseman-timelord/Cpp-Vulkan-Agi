4a94e9f56121ca42a7d5e84241f434b6076f66a9
Mister Maximus <92948927+wiseman-timelord@users.noreply.github.com>
2024-06-30 09:53:06 +0100

    selective update to => 0.20

---
+++ data/configure_temporary.py
@@ -0,0 +1,8 @@
+# .\data\configure_temporary.py
+
+current_project_plan = "Project Plan:\n1. Get details of new project, for the session."
+current_task_assigned = "Current Task:\nCommunicate with user, find out the details of the project."
+chat_model = None
+instruct_model = None
+code_model = None
+max_memory_usage = 90
